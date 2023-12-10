bf6dfac1e90a3cb95f71c428594268506357428b
neerajlifechange <144465632+neerajlifechange@users.noreply.github.com>
2023-12-10 14:02:32 +0530

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# Install playwright
+subprocess.run(["pip", "install", "playwright"])
+
+# Run playwright installation
+subprocess.run(["playwright", "install"])
+
+# Install getindianname
+subprocess.run(["pip", "install", "getindianname"])
